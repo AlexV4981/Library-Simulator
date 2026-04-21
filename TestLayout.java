aa22496fb1e9cdea9518e75ccc7bf8a9d38ed05c
Alex V3281 <145633682+AlexV4981@users.noreply.github.com>
2026-04-20 19:50:14 -0500

    Example Window
    Example Window

---
+++ TestLayout.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class TestLayout extends JFrame {
+    private JPanel contentPane;
+    private JTextField bookNameField;
+    private JTextField authorNameField;
+    private JComboBox genreComboBox;
+    private JCheckBox isTakenCheckBox;
+    private JButton cancelButton;
+    private JButton confirmButton;
+}
+
+
