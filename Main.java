b19bebf1a721ee0b2fbc55bd54ac621fa48d74d3
Alex V3281 <145633682+AlexV4981@users.noreply.github.com>
2026-04-30 20:18:55 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,28 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        // Setup Library
+        //Setup Library
         Library_Sim library = new Library_Sim("City Library", "123 Main St");
         
-        // Setup Shelves
+        //Setup Shelves
         Shelf fantasyShelf = new Shelf("Fantasy Shelf", "Fantasy", 3, 4);
         Shelf scienceShelf = new Shelf("Science Shelf", "Science", 6, 7);
         Shelf miscellaneousShelf = new Shelf("Miscellaneous Shelf", "Miscellaneous", 5, 5);
 
-        // Add Shelves via Librarian
+        //Add Shelves via Librarian
         Librarian headLibrarian = new Librarian("Alice");
         headLibrarian.addShelf(library, fantasyShelf);
         headLibrarian.addShelf(library, scienceShelf);
         headLibrarian.addShelf(library, miscellaneousShelf);
 
-        // Create Books
+        //Create Books
         Book book1 = new Book("The Hobbit", "J.R.R. Tolkien", 1937, "Fantasy");
         Book book2 = new Book("A Brief History of Time", "Stephen Hawking", 1988, "Science");
 
-        // Librarian adds books to the library (which distributes them to shelves)
+        //Librarian adds books to the library (which distributes them to shelves)
         headLibrarian.addBook(library, book1);
         headLibrarian.addBook(library, book2);
 
-        // Print Info
+        //Print Info
         System.out.println("--- Library Shelves ---");
         library.shelfInfo();
         
