11594106fb850e3ef0fcff0e56f3da299526a7ed
Alex V3281 <145633682+AlexV4981@users.noreply.github.com>
2026-04-30 20:20:41 -0500

    Update User.java

--- User.java
+++ User.java
@@ -1,23 +1,23 @@
 import java.util.ArrayList;
 
 public class User {
-    protected String name; // [cite: 70]
-    protected int ID; // [cite: 70]
-    protected ArrayList<Book> books; // 
+    protected String name;
+    protected int ID;
+    protected ArrayList<Book> books;
 
-    public User(String name) { // 
+    public User(String name) {
         this.name = name;
         this.books = new ArrayList<Book>();
     }
 
-    public void checkOut(Book book, String title) { // [cite: 73]
+    public void checkOut(Book book, String title) {
         if (book != null) {
             book.checkOut();
             books.add(book);
         }
     }
 
-    public void checkIn(Book book, String title) { // [cite: 74]
+    public void checkIn(Book book, String title) {
         if (book != null) {
             book.checkIn();
             books.remove(book);
